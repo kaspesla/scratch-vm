3abddba92217ebcdfd85ba781a8d14839f6d734d
James <student3@codeandcircuit.org>
2022-08-15 10:57:48 -0400

    video feed

---
+++ src/extensions/scratch3_spot/SpotVideoFeed.js
@@ -0,0 +1,82 @@
+
+const test = {"TEST": "TEST"};
+
+export class SpotVideoFeed {
+    constructor(runtime) {
+        this.runtime = runtime;
+        this.videoIsRunning = false;
+        this.socket = null;
+        this._workspace = [];
+        this._image = new Image();
+
+        
+    }
+
+    enableVideo() {
+        this.videoIsRunning = true;
+        this.socket = new WebSocket("ws://192.168.4.55:8000/ws/");
+
+        this.socket.onmessage = (message) => {
+            var data = JSON.parse(message['data']);
+            if (data['type'] == "@front") {
+                var image = data["output"];
+                this._image.src = "data:image/jpeg;base64," + image;
+            }
+        }
+        
+        $(window).on("beforeunload", () => {
+            try {
+                socket.send(
+                    JSON.stringify({
+                        action: "unload",
+                    })
+                );
+            }
+            catch(err) {
+                console.log(err);
+            }
+        });
+
+        return new Promise(resolve => resolve());
+    }
+
+    disableVideo() {
+        this.videoIsRunning = false;
+        return new Promise(resolve => resolve());
+    }
+
+    getFrame({dimensions, mirror, format, cacheTimeout}) {
+        const workspace = this._getWorkspace({dimensions: [640, 480], mirror: false});
+        const {canvas, context, lastUpdate, cacheData} = workspace; 
+        context.drawImage(this._image,
+            // source x, y, width, height
+            0, 0, this._image.width, this._image.height,
+            // dest x, y, width, height
+            0, 0, 640, 480
+        );
+        return canvas
+    }
+
+    _getWorkspace ({dimensions, mirror}) {
+        let workspace = this._workspace.find(space => (
+            space.dimensions.join('-') === dimensions.join('-') &&
+            space.mirror === mirror
+        ));
+        if (!workspace) {
+            workspace = {
+                dimensions,
+                mirror,
+                canvas: document.createElement('canvas'),
+                lastUpdate: 0,
+                cacheData: {}
+            };
+            workspace.canvas.width = dimensions[0];
+            workspace.canvas.height = dimensions[1];
+            workspace.context = workspace.canvas.getContext('2d');
+            this._workspace.push(workspace);
+        }
+        return workspace;
+    }
+
+
+}
